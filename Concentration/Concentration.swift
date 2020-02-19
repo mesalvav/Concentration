1268d390a5faeb236050e93578d25fb74ac22135
mario salvatierra <msalv006@fiu.edu>
2020-02-19 11:10:37 -0500

    video3 added computed properties 36min

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,7 +12,29 @@ class Concentration
 {
     
     var cards = [Card]()
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+    
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+        
+    }
     
     // play the game
     //
@@ -26,13 +48,13 @@ class Concentration
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
+                // indexOfOneAndOnlyFaceUpCard = nil
             } else {
                 // either no cards or 2 cards face up
-                for flipDownindex in cards.indices {
-                    cards[flipDownindex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
+//                for flipDownindex in cards.indices {
+//                    cards[flipDownindex].isFaceUp = false
+//                }
+//                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
