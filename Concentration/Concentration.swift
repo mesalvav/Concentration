ae4efd8650c1fa9e1fda7cd2aa7ae86bef8fda77
mario salvatierra <msalv006@fiu.edu>
2020-02-20 15:03:53 -0500

    video4 1:21min closure for oneAndOnly faceUp card

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -16,17 +16,18 @@ struct Concentration
     private var indexOfOneAndOnlyFaceUpCard: Int? {
     
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter( {cards[$0].isFaceUp}).oneAndOnly
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         
         set {
@@ -79,5 +80,11 @@ struct Concentration
             cards.append(appendThisCardAtEnd)
             //print(card.identifier)
         }
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
     }
 }
