4b2c02fa0d8c324d2625525dbbe925c869d97ac0
mario salvatierra <msalv006@fiu.edu>
2020-03-02 15:33:40 -0500

    video7 44min added themes

--- Concentration/ConcentrationThemeChooseViewController.swift
+++ Concentration/ConcentrationThemeChooseViewController.swift
@@ -15,16 +15,31 @@ class ConcentrationThemeChooseViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    // MARK: - Themes
     
-
-    /*
+    let themes = [
+        "Sports":"⚽️,🏀,🏈,⚾️,🎾,🏐,🏉,🎱,🏓,⛷,🎳,⛳️",
+        "Animals":"🐶,🐔,🦊,🐼,🦀,🐪,🐓,🐋,🐙,🦄,🐵",
+        "Faces":"😀,😂,😎,😫,😰,😴,🙄,🤔,😘,😁"
+    ]
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        if segue.identifier == "Choose Theme" {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                if let cvc = segue.destination as? ConcentrationViewController {
+                    cvc.theme = theme
+                }
+            }
+            
+        }
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
+    
 }
--- Concentration/ConcentrationViewController.swift
+++ Concentration/ConcentrationViewController.swift
@@ -54,17 +54,21 @@ class ConcentrationViewController: UIViewController {
     }
     
     private func updateViewFromModel(){
-        for index in cardButtons.indices {
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            if card.isFaceUp {
-                button.setTitle(emoji(for: card), for: .normal)
-                button.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-            } else {
-                button.setTitle("", for: .normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) :#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        if cardButtons != nil {
+            for index in cardButtons.indices {
+                let button = cardButtons[index]
+                let card = game.cards[index]
+                if card.isFaceUp {
+                    button.setTitle(emoji(for: card), for: .normal)
+                    button.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+                } else {
+                    button.setTitle("", for: .normal)
+                    button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) :#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+                }
             }
         }
+        
+        
     }
     
     var theme: String? {
